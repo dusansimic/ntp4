7cb764a2bad4bb8140b1c743fa6b183815886a11
Dušan Simić <dusan.simic1810@gmail.com>
2018-10-10 12:10:37 +0200

    Dodat invalid input poruka

--- forma-tabovi/index.js
+++ forma-tabovi/index.js
@@ -9,11 +9,24 @@ function onLoginFormSubmit() {
 	let database = storage.getItem('database');
 	database = database === null ? [] : JSON.parse(database);
 
+	let usernameFound = false;
 	for (const user of database) {
-		if (JSON.stringify(user) === JSON.stringify(inputuser)) {
-			location.assign('data.html')
-			return false;
+		if (user.username === inputuser.username) {
+			usernameFound = true;
+			if (user.password === inputuser.password) {
+				location.assign('data.html')
+				return false;
+			}
 		}
+	}
+	if (!usernameFound) {
+		$('#usernameInputLogin').removeClass('is-valid');
+		$('#usernameInputLogin').addClass('is-invalid');
+	} else {
+		$('#usernameInputLogin').removeClass('is-invalid');
+		$('#usernameInputLogin').addClass('is-valid');
+		$('#passwordInputLogin').removeClass('is-valid');
+		$('#passwordInputLogin').addClass('is-invalid');
 	}
 	console.log('not logged in');
 
@@ -21,11 +34,6 @@ function onLoginFormSubmit() {
 }
 
 function onRegisterFormSubmit() {
-	if ($('#passwordInputRegister').val() !== $('#repeatPasswordInputRegister').val()) {
-		console.log('Passwords don\'t match!');
-		return false;
-	}
-
 	const inputuser = {
 		username: $('#usernameInputRegister').val(),
 		password: sha256($('#passwordInputRegister').val())
@@ -34,11 +42,27 @@ function onRegisterFormSubmit() {
 	let database = storage.getItem('database');
 	database = database === null ? [] : JSON.parse(database);
 
+	let usernameInUse = false;
 	for (const user of database) {
 		if (user.username === inputuser.username) {
-			console.log(`Username ${inputuser.username} is already in use.`);
+			$('#usernameInputRegister').removeClass('is-valid');
+			$('#usernameInputRegister').addClass('is-invalid');
 			return false;
 		}
+	}
+	$('#usernameInputRegister').removeClass('is-invalid');
+	$('#usernameInputRegister').addClass('is-valid');
+	if ($('#passwordInputRegister').val() !== $('#repeatPasswordInputRegister').val()) {
+		$('#passwordInputRegister').removeClass('is-valid');
+		$('#passwordInputRegister').addClass('is-invalid');
+		$('#repeatPasswordInputRegister').removeClass('is-valid');
+		$('#repeatPasswordInputRegister').addClass('is-invalid');
+		return false;
+	} else {
+		$('#passwordInputRegister').removeClass('is-invalid');
+		$('#passwordInputRegister').addClass('is-valid');
+		$('#repeatPasswordInputRegister').removeClass('is-invalid');
+		$('#repeatPasswordInputRegister').addClass('is-valid');
 	}
 	database = [...database, inputuser];
 	storage.setItem('database', JSON.stringify(database));
